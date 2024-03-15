c497d7505b3421c0b7eb0c817048aeeeababecc1
corruptmane <corruptmane@gmail.com>
2024-03-15 15:29:59 +0200

    feat: `is_request_inbox` and `inbox_id` cached properties added for client `request` method

--- natsio/protocol/operations/hmsg.py
+++ natsio/protocol/operations/hmsg.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import lru_cache
 from typing import Final, Mapping, Optional
 
 from natsio.abc.protocol import ServerMessageProto
@@ -6,7 +7,7 @@ from natsio.abc.protocol import ServerMessageProto
 HMSG_OP: Final[bytes] = b"HMSG"
 
 
-@dataclass
+@dataclass(eq=False)
 class HMsg(ServerMessageProto):
     subject: str
     sid: str
@@ -15,6 +16,16 @@ class HMsg(ServerMessageProto):
     reply_to: Optional[str] = None
     headers: Optional[Mapping[str, str]] = None
     payload: Optional[bytes] = None
+
+    @property
+    @lru_cache
+    def is_request_inbox(self) -> bool:
+        return self.subject.startswith("_REQ_INBOX.")
+
+    @property
+    @lru_cache
+    def inbox_id(self) -> str:
+        return self.subject.split(".", maxsplit=1)[1]
 
 
 __all__ = (
--- natsio/protocol/operations/msg.py
+++ natsio/protocol/operations/msg.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import lru_cache
 from typing import Final, Optional
 
 from natsio.abc.protocol import ServerMessageProto
@@ -6,13 +7,23 @@ from natsio.abc.protocol import ServerMessageProto
 MSG_OP: Final[bytes] = b"MSG"
 
 
-@dataclass
+@dataclass(eq=False)
 class Msg(ServerMessageProto):
     subject: str
     sid: str
     payload_size: int
     reply_to: Optional[str] = None
     payload: Optional[bytes] = None
+
+    @property
+    @lru_cache
+    def is_request_inbox(self) -> bool:
+        return self.subject.startswith("_REQ_INBOX.")
+
+    @property
+    @lru_cache
+    def inbox_id(self) -> str:
+        return self.subject.split(".", maxsplit=1)[1]
 
 
 __all__ = (
