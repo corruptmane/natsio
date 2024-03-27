6d51c08f422f7e898f6896222e0c7b9375ed3300
corruptmane <corruptmane@gmail.com>
2024-03-27 03:12:13 +0200

    feat: render `ServerInfo` from `Info` server response

--- natsio/protocol/operations/info.py
+++ natsio/protocol/operations/info.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Final, List, Optional
 
 from natsio.abc.protocol import ServerMessageProto
+from natsio.client.config import ServerInfo
 
 INFO_OP: Final[bytes] = b"INFO"
 
@@ -32,6 +34,35 @@ class Info(ServerMessageProto):
     nonce: Optional[str] = None
     cluster: Optional[str] = None
     domain: Optional[str] = None
+
+    @cached_property
+    def server_info(self) -> ServerInfo:
+        return ServerInfo(
+            server_id=self.server_id,
+            server_name=self.server_name,
+            version=self.version,
+            go=self.go,
+            host=self.host,
+            port=self.port,
+            headers=self.headers,
+            max_payload=self.max_payload,
+            proto=self.proto,
+            client_id=self.client_id,
+            auth_required=self.auth_required,
+            tls_required=self.tls_required,
+            tls_verify=self.tls_verify,
+            tls_available=self.tls_available,
+            connect_urls=self.connect_urls,
+            ws_connect_urls=self.ws_connect_urls,
+            ldm=self.ldm,
+            git_commit=self.git_commit,
+            jetstream=self.jetstream,
+            ip=self.ip,
+            client_ip=self.client_ip,
+            nonce=self.nonce,
+            cluster=self.cluster,
+            domain=self.domain,
+        )
 
 
 __all__ = (
