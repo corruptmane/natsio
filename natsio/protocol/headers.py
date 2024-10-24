58dcef7625ed1ebe521c3d1dc2e26332c07f0953
corruptmane <corruptmane@gmail.com>
2024-10-24 22:54:49 +0300

    feat: nats const headers

---
+++ natsio/protocol/headers.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class Header(str, Enum):
+    MSG_ID = "Nats-Msg-Id"
+    EXPECTED_STREAM = "Nats-Expected-Stream"
+    EXPECTED_LAST_MSG_ID = "Nats-Expected-Last-Msg-Id"
+    EXPECTED_LAST_SEQUENCE = "Nats-Expected-Last-Sequence"
+    EXPECTED_LAST_SUBJECT_SEQUENCE = "Nats-Expected-Last-Subject-Sequence"
+    ROLLUP = "Nats-Rollup"
+    STREAM = "Nats-Stream"
+    SUBJECT = "Nats-Subject"
+    SEQUENCE = "Nats-Sequence"
+    LAST_SEQUENCE = "Nats-Last-Sequence"
+    TIMESTAMP = "Nats-Time-Stamp"
+    STREAM_SOURCE = "Nats-Stream-Source"
+    MSG_SIZE = "Nats-Msg-Size"
+    LAST_CONSUMER = "Nats-Last-Consumer"
+    LAST_STREAM = "Nats-Last-Stream"
+    CONSUMER_STALLED = "Nats-Consumer-Stalled"
+    STATUS = "Status"
+    DESCRIPTION = "Description"
