86c3f5938a1ae5102d8de84017a42fcaa33f24bf
corruptmane <corruptmane@gmail.com>
2024-11-06 00:03:40 +0200

    feat+fix: `from_msg_headers` classmethod | match case on strings instead of ints

--- natsio/exceptions/jetstream.py
+++ natsio/exceptions/jetstream.py
@@ -1,16 +1,19 @@
-from typing import Any
+from typing import Any, Mapping
 from natsio.exceptions.base import NATSError
+from natsio.protocol.headers import Header, StatusCode
 
 
 class APIError(NATSError):
     def __init__(
         self,
-        code: int | None = None,
+        code: str | int | None = None,
         description: str | None = None,
         err_code: int | None = None,
         stream: str | None = None,
         seq: int | None = None,
     ) -> None:
+        if code is not None:
+            code = int(code)
         self.code = code
         self.err_code = err_code
         self.description = description or ""
@@ -24,17 +27,25 @@ class APIError(NATSError):
         code = data["code"]
         err_class: type[APIError]
         match code:
-            case 400:
+            case "400":
                 err_class = BadRequestError
-            case 404:
+            case "404":
                 err_class = NotFoundError
-            case 500:
+            case "500":
                 err_class = ServerError
-            case 503:
+            case "503":
                 err_class = ServiceUnavailableError
             case _:
                 err_class = APIError
         return err_class(**data)
+
+    @classmethod
+    def from_msg_headers(cls, headers: Mapping[str, str] | None) -> "APIError":
+        if not headers:
+            return cls()
+        if Header.STATUS in headers and StatusCode.SERVICE_UNAVAILABLE:
+            return ServiceUnavailableError(code=503)  # pyright: ignore[reportReturnType]
+        return cls(code=int(headers[Header.STATUS]), description=headers[Header.DESCRIPTION])
 
     def __str__(self) -> str:
         return (
