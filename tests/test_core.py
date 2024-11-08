8c82d33cec3166c66d5dec97a16f20b604b11a31
corruptmane <corruptmane@gmail.com>
2024-11-08 23:19:39 +0200

    refactor: use futures instead of sleeps where it is usable | delete separate `partial` test as `partial` is used in another tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -22,16 +22,19 @@ async def test_core_subscription_async(nats_uri: list[str]) -> None:
         subject: Final[str] = "test.core.async"
         messages_count: Final[int] = 5
         received_messages: list[CoreMsg] = []
+        all_messages_received: asyncio.Future[None] = asyncio.Future()
 
         async def callback(msg: CoreMsg) -> None:
             received_messages.append(msg)
+            if len(received_messages) == messages_count and not all_messages_received.done():
+                all_messages_received.set_result(None)
 
         await nc.subscribe(subject, callback=callback)
 
         for i in range(messages_count):
             await nc.publish(subject, f"Hello, async: {i}".encode())
 
-        await asyncio.sleep(0.1)
+        await asyncio.wait_for(all_messages_received, timeout=1)
 
         assert len(received_messages) == messages_count
         for index, i in enumerate(range(messages_count)):
@@ -92,20 +95,20 @@ async def test_core_multiple_subscriptions_async_queue(nats_uri: list[str]) -> N
         messages_count: Final[int] = 5
         received_messages_1: list[CoreMsg] = []
         received_messages_2: list[CoreMsg] = []
+        all_messages_received: asyncio.Future[None] = asyncio.Future()
 
-        async def callback_1(msg: CoreMsg) -> None:
-            received_messages_1.append(msg)
+        async def callback(msg: CoreMsg, messages_collection: list[CoreMsg]) -> None:
+            messages_collection.append(msg)
+            if (len(received_messages_1) + len(received_messages_2)) == messages_count and not all_messages_received.done():
+                all_messages_received.set_result(None)
 
-        async def callback_2(msg: CoreMsg) -> None:
-            received_messages_2.append(msg)
-
-        await nc.subscribe(subject, queue, callback=callback_1)
-        await nc.subscribe(subject, queue, callback=callback_2)
+        await nc.subscribe(subject, queue, callback=partial(callback, messages_collection=received_messages_1))
+        await nc.subscribe(subject, queue, callback=partial(callback, messages_collection=received_messages_2))
 
         for i in range(messages_count):
             await nc.publish(subject, f"Hello, async queue: {i}".encode())
 
-        await asyncio.sleep(0.1)
+        await asyncio.wait_for(all_messages_received, timeout=1)
 
         assert len(received_messages_1) != len(received_messages_2)
         assert len(received_messages_1 + received_messages_2) == messages_count
@@ -118,20 +121,20 @@ async def test_core_multiple_subscriptions_async_noqueue(nats_uri: list[str]) ->
         messages_count: Final[int] = 5
         received_messages_1: list[CoreMsg] = []
         received_messages_2: list[CoreMsg] = []
+        all_messages_received: asyncio.Future[None] = asyncio.Future()
 
-        async def callback_1(msg: CoreMsg) -> None:
-            received_messages_1.append(msg)
+        async def callback(msg: CoreMsg, messages_collection: list[CoreMsg]) -> None:
+            messages_collection.append(msg)
+            if (len(received_messages_1) + len(received_messages_2)) == messages_count * 2 and not all_messages_received.done():
+                all_messages_received.set_result(None)
 
-        async def callback_2(msg: CoreMsg) -> None:
-            received_messages_2.append(msg)
-
-        await nc.subscribe(subject, callback=callback_1)
-        await nc.subscribe(subject, callback=callback_2)
+        await nc.subscribe(subject, callback=partial(callback, messages_collection=received_messages_1))
+        await nc.subscribe(subject, callback=partial(callback, messages_collection=received_messages_2))
 
         for i in range(messages_count):
             await nc.publish(subject, f"Hello, async noqueue: {i}".encode())
 
-        await asyncio.sleep(0.1)
+        await asyncio.wait_for(all_messages_received, timeout=1)
 
         assert len(received_messages_1) == len(received_messages_2)
         assert len(received_messages_1 + received_messages_2) == messages_count * 2
@@ -184,10 +187,10 @@ async def test_core_subscription_pending_bytes_limit(nats_uri: list[str]) -> Non
 async def test_core_subscription_unsub(nats_uri: list[str]) -> None:
     async with NATSCore(ClientConfig(servers=nats_uri)) as nc:
         subject: Final[str] = "test.core.unsub"
-        received_messages: list[CoreMsg] = []
+        message_received = asyncio.Event()
 
         async def callback(msg: CoreMsg) -> None:
-            received_messages.append(msg)
+            message_received.set()
 
         sub = await nc.subscribe(subject, callback=callback)
 
@@ -196,9 +199,9 @@ async def test_core_subscription_unsub(nats_uri: list[str]) -> None:
         for _ in range(5):
             await nc.publish(subject, b"unsub")
 
-        await asyncio.sleep(1)
-
-        assert not received_messages
+        with pytest.raises(TimeoutError):
+            await asyncio.wait_for(message_received.wait(), timeout=1)
+            pytest.fail("Received a message after unsubscribing")
 
 
 @pytest.mark.asyncio()
@@ -207,18 +210,23 @@ async def test_core_subscription_auto_unsub(nats_uri: list[str]) -> None:
         subject: Final[str] = "test.core.auto_unsub"
         unsub_after: Final[int] = 5
         received_messages: list[CoreMsg] = []
+        all_messages_received: asyncio.Future[None] = asyncio.Future()
 
         async def callback(msg: CoreMsg) -> None:
             received_messages.append(msg)
+            if len(received_messages) == unsub_after and not all_messages_received.done():
+                all_messages_received.set_result(None)
 
         sub = await nc.subscribe(subject, callback=callback)
-
         await sub.unsubscribe(max_msgs=unsub_after)
 
         for _ in range(10):
             await nc.publish(subject, b"unsub")
 
-        await asyncio.sleep(1)
+        await asyncio.wait_for(all_messages_received, timeout=1)
+
+        # Ensure no more messages are received after unsubscribing 
+        await asyncio.sleep(0.1)
 
         assert len(received_messages) == unsub_after
 
@@ -246,36 +254,20 @@ async def test_core_subscription_wildcard(nats_uri: list[str]) -> None:
         publish_subject_template: Final[str] = "test.core.wildcard.{part}"
         messages_count: Final[int] = 5
         received_messages: list[CoreMsg] = []
+        all_messages_received: asyncio.Future[None] = asyncio.Future()
 
         async def callback(msg: CoreMsg) -> None:
             received_messages.append(msg)
+            if len(received_messages) == messages_count and not all_messages_received.done():
+                all_messages_received.set_result(None)
 
         await nc.subscribe(subscription_subject, callback=callback)
 
         for i in range(messages_count):
             await nc.publish(publish_subject_template.format(part=i), b"something")
 
-        await asyncio.sleep(0.1)
+        await asyncio.wait_for(all_messages_received, timeout=1)
+
         assert len(received_messages) == messages_count
         for index, i in enumerate(range(messages_count)):
             assert received_messages[index].subject.endswith(str(i))
-
-
-@pytest.mark.asyncio()
-async def test_core_subscription_partial_callback(nats_uri: list[str]) -> None:
-    async with NATSCore(ClientConfig(servers=nats_uri)) as nc:
-        subject: Final[str] = "test.core.partial"
-        future: asyncio.Future[bytes] = asyncio.Future()
-        data: Final[bytes] = b"Partial test"
-
-        async def callback(msg: CoreMsg, part: Callable[[bytes], None]) -> None:
-            part(msg.payload)
-
-        callback = partial(callback, part=lambda data: future.set_result(data))
-
-        await nc.subscribe(subject, callback=callback)
-
-        await nc.publish(subject, data)
-
-        await future
-        assert future.result() == data
